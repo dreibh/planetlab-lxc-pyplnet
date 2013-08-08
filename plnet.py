4a728f79b1a701d5a626dee0e4a8171df785100d
Andy Bavier <acb@cs.princeton.edu>
2013-08-08 16:46:58 -0400

    Add tags for setting up OvS bridges

--- plnet.py
+++ plnet.py
@@ -124,7 +124,7 @@ def InitInterfaces(logger, plc, data, root="", files_only=False, program="NodeMa
 
             for setting in settings:
                 settingname = setting[name_key].upper()
-                if settingname in ('IFNAME','ALIAS','CFGOPTIONS','DRIVER','VLAN'):
+                if settingname in ('IFNAME','ALIAS','CFGOPTIONS','DRIVER','VLAN','TYPE','DEVICETYPE'):
                     details[settingname]=setting['value']
                 # IPv6 support on IPv4 interface
                 elif settingname in ('IPV6ADDR','IPV6_DEFAULTGW','IPV6ADDR_SECONDARIES'):
